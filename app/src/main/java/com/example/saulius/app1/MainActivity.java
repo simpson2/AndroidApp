4bfd45a6476102134941f9cdefcf444188752979
Saulius Baltramonaitis <msnfan234@gmail.com>
2018-09-27 19:25:59 +0100

    Show toast if input field empty

--- app/src/main/java/com/example/saulius/app1/MainActivity.java
+++ app/src/main/java/com/example/saulius/app1/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.saulius.app1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+
+        EditText inputMessage = findViewById(R.id.textMessage);
+        String tMessage = inputMessage.getText().toString();
+
+        if(tMessage.equals("")) {
+            Toast.makeText(this, "Please enter text before proceeding.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
