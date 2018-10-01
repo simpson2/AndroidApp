7876a019cd9e46afe3c65fd6891a5e21d9e328bf
Saulius Baltramonaitis <msnfan234@gmail.com>
2018-10-01 13:17:36 +0100

    remove redundant code

--- app/src/main/java/com/example/saulius/app1/SecondActivity.java
+++ app/src/main/java/com/example/saulius/app1/SecondActivity.java
@@ -14,7 +14,6 @@ public class SecondActivity extends AppCompatActivity {
 
     }
 
-    //private static final String TEXT_OUT = "text_out";
     public void showText() {
 
         TextView dispText = findViewById(R.id.dispText);
